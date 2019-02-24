adb45da8c9fc7a8c0e723330312ea081efc4e7ce
steelvoltage <steelvoltage@gmail.com>
2019-02-24 11:56:39 -0500

    Updates

---
+++ precache-manifest.664dce26cdc992202ca77748a1c03ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a2b72dc65ca3d7d4d65",
+    "url": "/io/contactmanager/static/js/runtime~main.3a2b72dc.js"
+  },
+  {
+    "revision": "4ce6bde8666526f3bbc3",
+    "url": "/io/contactmanager/static/js/main.4ce6bde8.chunk.js"
+  },
+  {
+    "revision": "153d7c42692a471b808a",
+    "url": "/io/contactmanager/static/js/2.153d7c42.chunk.js"
+  },
+  {
+    "revision": "4ce6bde8666526f3bbc3",
+    "url": "/io/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "153d7c42692a471b808a",
+    "url": "/io/contactmanager/static/css/2.3633f21e.chunk.css"
+  },
+  {
+    "revision": "7a2efa40da285e28053146c7de9424e0",
+    "url": "/io/contactmanager/index.html"
+  }
+];
